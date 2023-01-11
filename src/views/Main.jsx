52fa4de29a37bc301001a71cfc633e2506aeb957
7iliama1 <7iliama1@gmail.com>
2023-01-11 15:47:58 +0200

    Beautiful facing-Done

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,16 +1,58 @@
+/* eslint-disable max-len */
 const React = require('react');
 const Layout = require('./Layout');
 
 module.exports = function Main({ admin }) {
   return (
     <Layout admin={admin}>
-      <div className="main-container container">
+      {/* <div className="main-container container">
 
         <div className="row row-cols-2 greeting-main">
           <h1 style={{ fontSize: '60px' }}>
             {admin ? `Привет, admin ${admin}` : 'Привет, codder'}
           </h1>
         </div>
+
+      </div> */}
+
+      <div id="parallax-shop-of-watch">
+
+        <div className="Logo">
+          <header>
+            <img src="/img/pexels-ruslan-alekso-1401847.jpg" width="100px" height="100px" align="left" alt="watch" />
+            <br />
+            <h1>МАГАЗИН НАРУЧНЫХ ЧАСОВ</h1>
+            <h3>ЭКСКЛЮЗИВНОЙ РУЧНОЙ РАБОТЫ</h3>
+            <p className="line-break1 margin-top-20" />
+          </header>
+        </div>
+
+        <section>
+          <div className="parallax-one">
+            <h2>АКТУАЛЬНЫЕ МОДЕЛИ</h2>
+            <h4>ДЛЯ УСПЕШНОЙ ЖИЗНИ</h4>
+          </div>
+        </section>
+
+        <section>
+          <div className="about">
+            <p2>О нас</p2>
+          </div>
+        </section>
+
+        <section>
+          <div className="block">
+            <p>
+              <span className="first-character sc">I</span>
+              Наш магазин с 2018 года специализируется на продаже оригинальных часов различным мировых брендов.
+              За это время нашими клиентами стали более 3 тысяч человек.
+              В нашем магазине представлении наиболее интересные с точки зрения соотношения цена / качество часы.
+              Основной костяк наших часов сотовляют часы американских брендов : Invicta, Aragon, S.Coifman, TechnoMarine и другие.
+              У нас вы можете найти как классические и повседневные наручные часы, так и часы по истине поражающие воображение своим дизайна.
+            </p>
+            <p className="line-break margin-top-20" />
+          </div>
+        </section>
 
       </div>
     </Layout>
